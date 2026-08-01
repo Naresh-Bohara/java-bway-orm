117958dcbcc1e8f93c05c55afcfb7cd4c5f46c8b
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-08-01 11:54:11 +0545

    orm basics learned

---
+++ src/main/java/com/model/Student.java
@@ -0,0 +1,56 @@
+package com.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student_table")
+public class Student {
+@Id	//PK
+@Column 
+@GeneratedValue //AI 
+private int id;
+@Column
+private String fname;
+@Column
+private String lname;
+@Column
+private int age;
+@Column
+private String college;
+
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getFname() {
+	return fname;
+}
+public void setFname(String fname) {
+	this.fname = fname;
+}
+public String getLname() {
+	return lname;
+}
+public void setLname(String lname) {
+	this.lname = lname;
+}
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+public String getCollege() {
+	return college;
+}
+public void setCollege(String college) {
+	this.college = college;
+}
+
+}
